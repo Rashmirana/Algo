d644cdc89e352d6aaef0c2d825432c9f019c4f4e
Kanchan <kthareja12@gmail.com>
2019-06-12 01:34:36 +0530

    added a new algo

---
+++ Arrays - 1d/Leaders_in_Array.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//code
+	int T;
+	cin >> T;
+	while(T--)
+	{
+	    int n;
+	    cin >> n;
+	    
+	    int arr[n];
+	    for(int  i = 0; i < n; i++)
+	    cin >> arr[i];
+	    
+	    int res[n] = {0};
+	    res[n-1] = 1;
+	    
+	    int greatest = arr[n-1];
+	    for(int i = n - 2 ; i >= 0; i--)
+	    {
+	        if(arr[i] >= greatest)
+	        {
+	            greatest = arr[i];
+	            res[i]++;
+	        }
+	    }
+	    
+	    for(int i = 0; i < n; i++)
+	    {
+    	    if(res[i] == 1)
+    	    cout << arr[i]<< " ";        
+	    }
+	    cout << endl;	    
+	}
+	return 0;
+}
