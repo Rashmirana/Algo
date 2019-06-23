a2fe70abd076cf48616e3b40cbf74221c0717b23
Rashmirana <ranarashmi8285@gmail.com>
2019-06-23 22:37:57 +0530

    Find Substring

---
+++ String/SubString.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class  SubString {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        System.out.println(S.substring(start , end));
+    }
+}
+/*
+Input:-
+helloworld
+3
+6
+
+Output:-
+low
+*/
